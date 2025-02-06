23ed88928ba8ceb84d9892835e335d66f466dd89
IrulesDev <ad4112ul@gmail.com>
2025-02-06 21:24:15 +0700

    tambah controller pengajuanLayanan

--- app/Http/Controllers/PengajuanLayananController.php
+++ app/Http/Controllers/PengajuanLayananController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PengajuanLayanan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PengajuanLayananController extends Controller
 {
@@ -11,7 +13,8 @@ class PengajuanLayananController extends Controller
      */
     public function index()
     {
-        //
+        $pengajuan = PengajuanLayanan::all();
+        return response()->json($pengajuan);
     }
 
     /**
@@ -19,7 +22,22 @@ class PengajuanLayananController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Log::info('Store method called');
+        Log::info($request->all());
+
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $pengajuan = PengajuanLayanan::create([
+            'name' => $request->name,
+        ]);
+
+        Log::info('Pengajuan created: ' . $pengajuan->id);
+
+       return response()->json([
+           'message' => 'Data ' . $pengajuan->name . ' Berhasil Ditambahkan' ,
+       ]);
     }
 
     /**
@@ -27,7 +45,12 @@ class PengajuanLayananController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $Pengajuan = PengajuanLayanan::find($id);
+
+        if (!$Pengajuan){
+            return response()->json(['message' => 'pengajuan not found'],404);
+        }
+        return response()->json($Pengajuan);
     }
 
     /**
@@ -35,7 +58,21 @@ class PengajuanLayananController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $pengajuan = PengajuanLayanan::find($id);
+
+        if (!$pengajuan){
+            return response()->json(['message' => 'pengajuan not found'],404);
+        }
+
+        $dataPengajuan = $request->validate([
+            'name' => 'required'
+        ]);
+
+        $pengajuan->update($dataPengajuan); //update data
+
+        return response()->json([
+            'message' => 'Data' . $pengajuan->name . 'Berhasil Diubah'
+        ]);
     }
 
     /**
@@ -43,6 +80,16 @@ class PengajuanLayananController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $pengajuan = PengajuanLayanan::find($id);
+
+        if (!$pengajuan){
+            return response()->json(['messege' => 'perangkat not found'],404);
+        }
+
+        $pengajuan->delete(); //delete data
+
+        return response()->json([
+            'message' => 'Data' . $pengajuan->name . 'Berasil Dihapus'
+        ]);
     }
 }
